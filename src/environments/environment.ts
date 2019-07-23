435599fb0997254a37aeffe393da1d536269039d
root <wattana.kd@gmail.com>
2019-07-24 00:31:58 +0700

    First commit before deploment

---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  serverUrl: 'https://foodbox.everfac.com/parse',
+  appUrl: 'https://foodbox.everfac.com',
+  appImageUrl: 'https://foodbox.everfac.com/assets/imgs/ionshop.png',
+  appId: 'SvflR3hB7O',
+  fbId: '463291427783760',
+  stripePublicKey: 'pk_test_rYNh8ELUCYssBH2rqScBFZmW00xKnJmqnP',
+  androidHeaderColor: '#5D67D4',
+  defaultLang: 'en',
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
