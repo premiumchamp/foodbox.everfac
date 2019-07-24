ffeca4707a02da6191452bbb7b283894a3e91a6f
premiumchamp <53110038+premiumchamp@users.noreply.github.com>
2019-07-24 22:48:53 +0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   serverUrl: 'https://foodbox.everfac.com/parse',
   appUrl: 'https://shop.everfac.com',
   appImageUrl: 'https://foodbox.everfac.com/assets/imgs/ionshop.png',
